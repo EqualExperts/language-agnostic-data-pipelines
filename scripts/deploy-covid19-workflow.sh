37cd1a838ce10ea3e5a520f8e32ab820b1bf8a58
Cláudio Diniz <cdiniz@equalexperts.com>
2021-03-28 11:53:17 +0100

    Rename components and update readme

--- scripts/deploy-covid19-workflow.sh
+++ scripts/deploy-covid19-workflow.sh
@@ -3,7 +3,7 @@
 # eval minikube docker env to make the local docker images available inside k8s cluster
 eval $(minikube docker-env)
 
-cd ../workflow-tasks/covid19-uk-ingestions 
+cd ../connectors/covid19-uk-ingestions 
 
 # create jar for db-migrations
 make create_uberjar
@@ -11,14 +11,14 @@ make create_uberjar
 cd ../../
 
 # create ingestion and transformation docker images
-docker build -f workflows/covid19-uk/Dockerfile.ingestion -t covid19ukingestions:latest .
-docker build -f workflows/covid19-uk/Dockerfile.transformation -t covid19dbt:latest .
+docker build -f data-pipelines/covid19-uk/Dockerfile.ingestion -t covid19ukingestions:latest .
+docker build -f data-pipelines/covid19-uk/Dockerfile.transformation -t covid19dbt:latest .
 
 # cleanup old argo template
 argo -n argo template delete covid19-uk
 
 # generate workflow using helm templating engine
-helm template covid19-uk workflows/covid19-uk --set environment=dev --set name=covid19-uk --output-dir outputs
+helm template covid19-uk data-pipelines/covid19-uk --set environment=dev --set name=covid19-uk --output-dir outputs
 
 # create workflow template on argo
 argo -n argo template create outputs/covid19-uk/templates/workflow-template.yaml
--- scripts/deploy-db-migrations-workflow.sh
+++ scripts/deploy-db-migrations-workflow.sh
@@ -3,7 +3,7 @@
 # eval minikube docker env to make the local docker images available inside k8s cluster
 eval $(minikube docker-env)
 
-cd ../workflow-tasks/covid19-uk-ingestions
+cd ../connectors/covid19-uk-ingestions
 
 # create jar for db-migrations
 make create_migrator_uberjar 
@@ -11,13 +11,13 @@ make create_migrator_uberjar
 cd ../../
 
 # create db-migrations image
-docker build -f workflows/db-migrations/Dockerfile -t db-migrations:latest .
+docker build -f data-pipelines/db-migrations/Dockerfile -t db-migrations:latest .
 
 # cleanup old runs
 argo -n argo delete db-migrations
 
 # generate workflow using helm templating engine
-helm template db-migrations workflows/db-migrations --set environment=dev --set name=db-migrations --output-dir outputs
+helm template db-migrations data-pipelines/db-migrations --set environment=dev --set name=db-migrations --output-dir outputs
 
 # submit db-migrations workflow to argo
 argo -n argo submit --log  outputs/db-migrations/templates/workflow.yaml
